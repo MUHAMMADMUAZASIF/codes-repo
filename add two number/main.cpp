18b2d65dc9707482a2899c32eb07c14061b07c4c
muaz <mmuazasif8@gmail.com>
2025-01-23 22:56:33 +0500

    add numbers

---
+++ add two number/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"value of a is ";
+    cin>>a;
+    cout<<"value of b is ";
+    cin>>b;
+    int sum;
+    sum=a+b;
+    cout<<"value of sum is:"<<sum<<endl;
+    return 0;
+}
